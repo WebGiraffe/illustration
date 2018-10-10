94d76c80eac4b21d34e25eeb7257f6c1f8d7b643
jiayingxiang <jiayingxiang@163.com>
2018-10-10 10:11:24 +0800

    active_competiton
    构造出初步的vue结构

---
+++ src/components/jyxHeader.vue
@@ -0,0 +1,154 @@
+<template>
+    <div>
+        <div class="nav">
+        <div class="nav-content">
+            <img src="../../public/img/jyx-active/logo4.png" alt="">
+            <div class="nav-content-item">
+                <ul>
+                    <li>
+                        <a href="#">作品</a>
+                        <div class="icon"></div>
+                        <div class="slide-list">
+                            <a href="#">最新发布</a>
+                            <a href="#">最新获赞</a>
+                            <a href="#">热门作品</a>
+                            <a href="#">作品精选</a>
+                            <a href="#">推荐作者</a>
+                        </div>
+                    </li>
+                    <li><a href="#">活动比赛</a></li>
+                    <li><a href="#">征稿</a></li>
+                    <li><a href="#">客户端</a></li>
+                    <li>
+                        <a href="#">更多</a>
+                        <div class="icon"></div>
+                        <div class="slide-list">
+                            <a href="#">合作关系</a>
+                            <a href="#">帮助中心</a>
+                            <a href="#">官方微博</a>
+                            <a href="#">艺场插画授权</a>
+                        </div>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <img style="height:35px;vertical-align: middle;" src="../../public/img/jyx-active/search.svg" alt="">
+                        </a>
+                    </li>
+                    <li><a href="#">插画课程直播</a></li>
+                </ul>
+            </div>
+            <div class="nav-content-item">
+                <a href="#">注册</a>
+                <a href="#">登录</a>
+            </div>
+        </div>
+        </div>
+    </div>
+</template>
+<script>
+    export default{
+        name:"jyxHeader",
+        data(){
+            return{}
+        }
+    }
+</script>
+<style scoped>
+    *{
+            padding:0;
+            margin: 0;
+            box-sizing: border-box;
+        }
+        div.nav{
+            background: #1A1A1A;
+            width: 100%;
+            height: 50px;
+        }
+        div.nav>div.nav-content{
+            width: 1130px;
+            height: 50px;
+            margin:0 auto;
+        }
+        div.nav>div.nav-content>img{
+            float: left;
+            width: 40px;
+            height: 40px;
+            margin-right:25px;
+        }
+        div.nav>div.nav-content>.nav-content-item:nth-child(2)>ul>li{
+            list-style: none;
+            float: left;
+            height: 50px;
+            padding: 0 16px 0 16px;
+            line-height: 50px;
+            position: relative;
+        }
+        div.nav-content-item:nth-child(2)>ul>li:nth-child(7)>a{
+            color: orange;
+        }
+        div.nav-content-item:nth-child(2)>ul>li:nth-child(7)>a:hover{
+            color: #777;
+        }
+        div.nav-content-item:nth-child(2)>ul>li>a{
+            text-decoration: none;
+            font-size: 14px;
+            color:#ccc;
+            cursor: pointer;
+        }
+        div.nav-content-item:nth-child(3){
+            float: right;
+            line-height: 50px;
+        }
+        div.nav-content-item:nth-child(3)>a{
+            text-decoration: none;
+            font-size: 12px;
+            color:#ccc;
+            padding-left: 12px;
+        }
+        div.nav-content-item:nth-child(3)>a:hover{
+            color: #fff;
+        }
+        div.nav-content-item:nth-child(2)>ul>li:first-child>div.icon,div.nav-content-item:nth-child(2)>ul>li:nth-child(5)>div.icon{
+            width:10px;
+            height:10px;
+            border-bottom:5px solid #fff;
+            border-left:5px solid transparent;
+            border-right:5px solid transparent;
+            border-top:5px solid transparent;
+            position: absolute;
+            top:40px;
+            left:26px;
+            visibility: hidden;
+        }
+        div.nav-content-item:nth-child(2)>ul>li:first-child>div.slide-list,div.nav-content-item:nth-child(2)>ul>li:nth-child(5)>div.slide-list{
+            width: 118px;
+            /*height: 170px;*/
+            text-align: center;
+            border:1px solid rgba(0, 0, 0, .1);
+            position: absolute;
+            top: 50px;
+            left: -27px;
+            background-color: #fff;
+            border-radius: 3px;
+            box-shadow: 0 3px 6px rgba(0,0,0,.1);
+            z-index:100;
+            visibility: hidden;
+        }
+        div.nav-content-item:nth-child(2)>ul>li:first-child>div.slide-list>a,div.nav-content-item:nth-child(2)>ul>li:nth-child(5)>div.slide-list>a{
+            text-decoration: none;
+            display: block;
+            line-height: 34px;
+            color: #333;
+            cursor:pointer;
+            font-size:14px;
+        }
+        div.nav-content-item:nth-child(2)>ul>li:first-child>div.slide-list>a:hover, div.nav-content-item:nth-child(2)>ul>li:nth-child(5)>div.slide-list>a:hover{
+            color: #777;
+        }
+        div.nav-content-item:nth-child(2)>ul>li:first-child:hover div.slide-list,div.nav-content-item:nth-child(2)>ul>li:nth-child(5):hover div.slide-list{
+            visibility:visible;
+        }
+        div.nav-content-item:nth-child(2)>ul>li:first-child:hover div.icon,div.nav-content-item:nth-child(2)>ul>li:nth-child(5):hover div.icon{
+            visibility: visible;
+        }
+</style>
---
+++ src/views/Active_competition.vue
@@ -0,0 +1,519 @@
+<template>
+    <div>
+        <jyxHeader></jyxHeader>
+        <div id="nav">
+        <div class="work-nav">
+            <div>
+                <ul class="wnav">
+                    <li><a href="#"><img src="img/jyx-active/home3.png" alt=""></a></li>
+                    <li><a href="#">全部</a></li>
+                    <li><a href="#">进行</a></li>
+                    <li><a href="#">已结束</a></li>
+                    <li><a href="#">我的资料</a></li>
+                </ul>
+                <ul class="wnav">
+                    <li><a href="#">公司介绍</a></li>
+                    <li><a href="#">活动合作QQ：2090677817</a></li>
+                </ul>
+            </div>
+        </div>
+        <div class="wrapper">
+            <a href="#" target="_blank">
+                <img src="img/jyx-active/pic_srydfvcj74t9oh5fsgb15bbx8lx36s5r.jpg" alt="">
+            </a>
+        </div>
+    </div>
+    <div id="fixBar">
+        <a href="#"></a>
+        <a>
+            <img src="img/jyx-active/xcx.jpg" alt="" style="width:75px;margin:1px 0 1px;">
+        </a>
+        <a href="#" style="padding:2px">微信小程序</a>
+    </div>
+    <div id="section">
+        <div class="list_wrap">
+            <ul>
+                <li>
+                    <div>
+                        <a href="#">
+                            <img src="img/jyx-active/pic_k7mvt9r2s0mj51a421yrb8w3vcti5914.jpg" alt="">
+                        </a>
+                    </div>
+                    <div>
+                        <div>
+                            <h3>笔绘宫心「宫廷计」手游同人插画漫画征集大赛</h3>
+                            <p>活动进行中</p>
+                        </div>
+                        <div>
+                            <div>
+                                <div class="date_box">16</div>
+                                <p>天</p>
+                            </div>
+                            <div>
+                                <div class="date_box">4</div>
+                                <p>时</p>
+                            </div>
+                            <div>
+                                <div class="date_box">44</div>
+                                <p>分</p>
+                            </div>
+                        </div>
+                    </div>
+                </li>
+                <li>
+                        <div>
+                                <a href="#">
+                                    <img src="img/jyx-active/pic_k7mvt9r2s0mj51a421yrb8w3vcti5914.jpg" alt="">
+                                </a>
+                            </div>
+                            <div>
+                                <div>
+                                    <h3>笔绘宫心「宫廷计」手游同人插画漫画征集大赛</h3>
+                                    <p>活动进行中</p>
+                                </div>
+                                <div>
+                                    <div>
+                                        <div class="date_box">16</div>
+                                        <p>天</p>
+                                    </div>
+                                    <div>
+                                        <div class="date_box">4</div>
+                                        <p>时</p>
+                                    </div>
+                                    <div>
+                                        <div class="date_box">44</div>
+                                        <p>分</p>
+                                    </div>
+                                </div>
+                            </div>
+                </li>
+                <li>
+                        <div>
+                                <a href="#">
+                                    <img src="img/jyx-active/pic_k7mvt9r2s0mj51a421yrb8w3vcti5914.jpg" alt="">
+                                </a>
+                            </div>
+                            <div>
+                                <div>
+                                    <h3>笔绘宫心「宫廷计」手游同人插画漫画征集大赛</h3>
+                                    <p>活动进行中</p>
+                                </div>
+                                <div>
+                                    <div>
+                                        <div class="date_box">16</div>
+                                        <p>天</p>
+                                    </div>
+                                    <div>
+                                        <div class="date_box">4</div>
+                                        <p>时</p>
+                                    </div>
+                                    <div>
+                                        <div class="date_box">44</div>
+                                        <p>分</p>
+                                    </div>
+                                </div>
+                            </div>
+                </li>
+                <li>
+                        <div>
+                                <a href="#">
+                                    <img src="img/jyx-active/pic_k7mvt9r2s0mj51a421yrb8w3vcti5914.jpg" alt="">
+                                </a>
+                            </div>
+                            <div>
+                                <div>
+                                    <h3>笔绘宫心「宫廷计」手游同人插画漫画征集大赛</h3>
+                                    <p>活动进行中</p>
+                                </div>
+                                <div>
+                                    <div>
+                                        <div class="date_box">16</div>
+                                        <p>天</p>
+                                    </div>
+                                    <div>
+                                        <div class="date_box">4</div>
+                                        <p>时</p>
+                                    </div>
+                                    <div>
+                                        <div class="date_box">44</div>
+                                        <p>分</p>
+                                    </div>
+                                </div>
+                            </div>
+                </li>
+                <li>
+                        <div>
+                                <a href="#">
+                                    <img src="img/jyx-active/pic_k7mvt9r2s0mj51a421yrb8w3vcti5914.jpg" alt="">
+                                </a>
+                            </div>
+                            <div>
+                                <div>
+                                    <h3>笔绘宫心「宫廷计」手游同人插画漫画征集大赛</h3>
+                                    <p>活动进行中</p>
+                                </div>
+                                <div>
+                                    <div>
+                                        <div class="date_box">16</div>
+                                        <p>天</p>
+                                    </div>
+                                    <div>
+                                        <div class="date_box">4</div>
+                                        <p>时</p>
+                                    </div>
+                                    <div>
+                                        <div class="date_box">44</div>
+                                        <p>分</p>
+                                    </div>
+                                </div>
+                            </div>
+                </li>
+                <li>
+                        <div>
+                                <a href="#">
+                                    <img src="img/jyx-active/pic_k7mvt9r2s0mj51a421yrb8w3vcti5914.jpg" alt="">
+                                </a>
+                            </div>
+                            <div>
+                                <div>
+                                    <h3>笔绘宫心「宫廷计」手游同人插画漫画征集大赛</h3>
+                                    <p>活动进行中</p>
+                                </div>
+                                <div>
+                                    <div>
+                                        <div class="date_box">16</div>
+                                        <p>天</p>
+                                    </div>
+                                    <div>
+                                        <div class="date_box">4</div>
+                                        <p>时</p>
+                                    </div>
+                                    <div>
+                                        <div class="date_box">44</div>
+                                        <p>分</p>
+                                    </div>
+                                </div>
+                            </div>
+                </li>
+                <li>
+                        <div>
+                                <a href="#">
+                                    <img src="img/jyx-active/pic_k7mvt9r2s0mj51a421yrb8w3vcti5914.jpg" alt="">
+                                </a>
+                            </div>
+                            <div>
+                                <div>
+                                    <h3>笔绘宫心「宫廷计」手游同人插画漫画征集大赛</h3>
+                                    <p>活动进行中</p>
+                                </div>
+                                <div>
+                                    <div>
+                                        <div class="date_box">16</div>
+                                        <p>天</p>
+                                    </div>
+                                    <div>
+                                        <div class="date_box">4</div>
+                                        <p>时</p>
+                                    </div>
+                                    <div>
+                                        <div class="date_box">44</div>
+                                        <p>分</p>
+                                    </div>
+                                </div>
+                            </div>
+                </li>
+                <li>
+                        <div>
+                                <a href="#">
+                                    <img src="img/jyx-active/pic_k7mvt9r2s0mj51a421yrb8w3vcti5914.jpg" alt="">
+                                </a>
+                            </div>
+                            <div>
+                                <div>
+                                    <h3>笔绘宫心「宫廷计」手游同人插画漫画征集大赛</h3>
+                                    <p>活动进行中</p>
+                                </div>
+                                <div>
+                                    <div>
+                                        <div class="date_box">16</div>
+                                        <p>天</p>
+                                    </div>
+                                    <div>
+                                        <div class="date_box">4</div>
+                                        <p>时</p>
+                                    </div>
+                                    <div>
+                                        <div class="date_box">44</div>
+                                        <p>分</p>
+                                    </div>
+                                </div>
+                            </div>
+                </li>
+                <li>
+                        <div>
+                                <a href="#">
+                                    <img src="img/jyx-active/pic_k7mvt9r2s0mj51a421yrb8w3vcti5914.jpg" alt="">
+                                </a>
+                            </div>
+                            <div>
+                                <div>
+                                    <h3>笔绘宫心「宫廷计」手游同人插画漫画征集大赛</h3>
+                                    <p>活动进行中</p>
+                                </div>
+                                <div>
+                                    <div>
+                                        <div class="date_box">16</div>
+                                        <p>天</p>
+                                    </div>
+                                    <div>
+                                        <div class="date_box">4</div>
+                                        <p>时</p>
+                                    </div>
+                                    <div>
+                                        <div class="date_box">44</div>
+                                        <p>分</p>
+                                    </div>
+                                </div>
+                            </div>
+                </li>
+                <li>
+                        <div>
+                                <a href="#">
+                                    <img src="img/jyx-active/pic_k7mvt9r2s0mj51a421yrb8w3vcti5914.jpg" alt="">
+                                </a>
+                            </div>
+                            <div>
+                                <div>
+                                    <h3>笔绘宫心「宫廷计」手游同人插画漫画征集大赛</h3>
+                                    <p>活动进行中</p>
+                                </div>
+                                <div>
+                                    <div>
+                                        <div class="date_box">16</div>
+                                        <p>天</p>
+                                    </div>
+                                    <div>
+                                        <div class="date_box">4</div>
+                                        <p>时</p>
+                                    </div>
+                                    <div>
+                                        <div class="date_box">44</div>
+                                        <p>分</p>
+                                    </div>
+                                </div>
+                            </div>
+                </li>
+            </ul>
+            <div class="page">
+                <a href="#">首页</a>
+                <a href="#">上页</a>
+                <a href="#" class="on">1</a>
+                <a href="#">2</a>
+                <a href="#">3</a>
+                <a href="#">4</a>
+                <a href="#">5</a>
+                <a href="#">下页</a>
+                <a href="#">尾页</a>
+            </div>
+        </div>
+    </div>
+    <div class="wrapper_bottom">
+            <a href="#" target="_blank">
+                <img src="img/jyx-active/pic_rbc5zndyvpa475j3o01gd4xw0lje8ywu.jpg" alt="">
+            </a>
+        </div>
+        <jyxFooter></jyxFooter>
+    </div>
+</template>
+<script>
+    import jyxHeader from "@/components/jyxHeader.vue";
+    import jyxFooter from "@/components/jyxFooter.vue";
+    export default{
+        data(){
+            return{}
+        },
+        components:{
+            jyxHeader,
+            jyxFooter
+        }
+    }
+</script>
+<style scoped>
+        *{
+            margin:0;
+            padding:0;
+            box-sizing: border-box;
+        }
+        body{
+            font-family: 微软雅黑;
+            font-size: 14px;
+            line-height: 24px;
+        }
+        body a{
+            text-decoration: none;
+            color:#000;
+        }
+        #nav>.work-nav{
+            width:100%;
+            height:50px;
+            background-color:#fff;
+            line-height: 50px;
+        }
+        #nav>.work-nav>div{
+            width:1120px;
+            height:50px;
+            margin:0 auto;
+        }
+        #nav>.work-nav>div>.wnav>li{
+            list-style: none;
+            padding:0 15px 0;
+        }
+        #nav>.work-nav>div>.wnav:first-child>li{
+            float:left;
+        }
+        #nav>.work-nav>div>.wnav:first-child>li:first-child>a>img{
+            width:16px;
+        }
+        #nav>.work-nav>div>.wnav:last-child>li{
+            float:right;
+        }
+        #nav>.work-nav>div>.wnav>li>a{
+            color:#444445;
+            font-size: 15px;
+        }
+        #nav>.wrapper{
+            width:100%;
+            background-color:#E6EAED;
+            text-align: center;
+        }
+        #nav>.wrapper>a>img{
+            width: 1120px;
+            height:90px;
+            margin:20px 0 20px 0;
+        }
+        #fixBar{
+            position: fixed;
+            right:20px;
+            bottom: 100px;
+            width: 80px;
+        }
+        #fixBar>a:first-child{
+            width: 80px;
+            height: 35px;
+            display: block;
+            border:1px solid #e7e7e7;
+            background:#fbfbfb url("../../public/img/jyx-active/top.jpg") no-repeat center;
+        }
+        #fixBar>a:nth-child(2){
+            width: 80px;
+            height: 80px;
+            display: block;
+            border:1px solid #e7e7e7;
+            background:#fff;
+        }
+        #fixBar>a:last-child{
+            display: block;
+            background: #fbfbfb;
+            border: 1px #e7e7e7 solid;
+            padding: 3px 5px;
+            font-size: 12px;
+            text-align: center;
+            line-height: 16px;
+            opacity: .9;
+            margin-top: 4px;
+            cursor: pointer;
+        }
+        #section{
+            width: 100%;
+            height: 2510px;
+        }
+        #section>.list_wrap{
+            width: 1120px;
+            height: 2510px;
+            margin: 0 auto;
+        }
+        #section>.list_wrap>ul{
+            overflow: hidden;
+            list-style: none;
+            margin: 0 -2px 0 -30px;
+            padding-top: 50px;
+        }
+        #section>.list_wrap>ul>li{
+            float:left;
+            width: 546px;
+            margin: 0 0 45px 30px;
+            padding:0 2px 2px 0;
+            color: #858585;
+        }
+        #section>.list_wrap>ul>li>div:first-child>a>img{
+            width: 100%;
+            cursor: pointer;
+        }
+        #section>.list_wrap>ul>li>div:last-child{
+            display: flex;
+            justify-content: space-between;
+        }
+        #section>.list_wrap>ul>li>div:last-child>div:first-child>h3{
+            overflow: hidden;
+            margin-top: 20px;
+            font-size: 18px;
+            font-weight: bold;
+            line-height: 27px;
+            color: #1a1a1a;
+        }
+        #section>.list_wrap>ul>li>div:last-child>div:first-child>p{
+            padding-top: 22px;
+            font-size: 15px;
+            font-weight: bold;
+            color: #00d084;
+        }
+        #section>.list_wrap>ul>li>div:last-child>div:last-child{
+            display: flex;
+            justify-content: space-between;
+            text-align: center;
+            margin-top: 20px;
+        }
+        #section>.list_wrap>ul>li>div:last-child>div:last-child>div>.date_box{
+            width: 46px;
+            height: 46px;
+            margin-bottom: 6px;
+            background: #fff;
+            border: 1px solid #e7eaed;
+            font-size: 28px;
+            letter-spacing: 1px;
+            line-height: 48px;
+            font-weight: bold;
+            color: #1a1a1a;
+        }
+        #section>.list_wrap>.page{
+            text-align: center;
+        }
+        #section>.list_wrap>.page>.on{
+            background: #ffbb33;
+            color: #fff;
+            border: 1px #ffbb33 solid;
+        }
+        #section>.list_wrap>.page>a{
+            line-height: 38px;
+            height: 38px;
+            padding: 0 15px;
+            color: #888;
+            border: 1px #d5d5d3 solid;
+            background: #fefefe;
+            margin: 0 2px;
+            border-radius: 5px;
+            display: inline-block;
+            font-size: 14px;
+        }
+        .wrapper_bottom{
+            width: 100%;
+            background-color:#E6EAED;
+            text-align: center;
+            margin-bottom:20px;
+        }
+        .wrapper_bottom>a>img{
+            width: 1120px;
+            height:90px;
+            margin:20px 0 10px 0;
+        }
+</style>
