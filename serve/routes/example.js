ce99e8d687f53eb7864e83ad2a93609a35fc550c
inling <giraffe.aup@gmail.com>
2018-10-11 19:39:19 +0800

    delete

--- serve/routes/example.js
+++
@@ -1,9 +0,0 @@
-const express=require('express');
-const pool=require('../pool');
-var router= express.Router();
-
-router.get('/',(req,res)=>{
-    res.send('hahhah');
-})
-
-module.exports=router;
